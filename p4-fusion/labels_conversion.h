684611fe134d569543bb5a637d9582a48fc37b97
Petri-Johan Last <petri.last@sourcegraph.com>
2024-07-25 11:35:28 +0200

    Don't export unnecessary functions

--- p4-fusion/labels_conversion.h
+++ p4-fusion/labels_conversion.h
@@ -2,9 +2,7 @@
 #define LABELS_CONVERSION_H
 #include "p4_api.h"
 
-int updateTags(P4API* p4, const std::string& depotPath, git_repository* repo);
 std::string convertLabelToTag(std::string label);
-std::string trimSuffix(const std::string& str, const std::string& suffix);
 std::string trimPrefix(const std::string& str, const std::string& prefix);
 std::string getChangelistFromCommit(const git_commit* commit);
 LabelMap getLabelsDetails(P4API* p4, std::string depotPath, std::list<std::string> labels);
